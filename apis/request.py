87ee453b3bf80d7f6b7da415c7b44f7c360330fc
Thandara Felix <thfelix@uolinc.com>
2024-08-27 07:46:01 -0300

    Ajustando request.py

--- apis/request.py
+++ apis/request.py
@@ -1,4 +1,7 @@
 import requests
+import argparse
+from requests.auth import HTTPBasicAuth
+
 url = 'http://127.0.0.1:5000/cotacao-completa/'
 
 
@@ -21,3 +24,12 @@ valor = response.json()
 print (response.status_code)
 
 print(valor)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Enviar dados para a API Flask.')
+    parser.add_argument('--tamanho', required=True, help='Tamanho da casa')
+    parser.add_argument('--ano', required=True, help='Ano de construção')
+    parser.add_argument('--garagem', required=True, help='Número de vagas na garagem')
+
+    args = parser.parse_args()
+    main(args.tamanho, args.ano, args.garagem)
